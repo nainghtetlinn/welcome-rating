cc9e3fc25c168c49cde64850194af0575158cdb3
nainghtetlinn <naing.work.it@gmail.com>
2025-03-20 23:33:27 +0630

    stable v1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,9 +15,20 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogDescription,
+//   DialogFooter,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogTrigger,
+// } from '@/components/ui/dialog'
+// import { Button } from '@/components/ui/button'
 import Chart from '../components/Chart'
 
 import { useRating } from '../Provider'
+import { useEffect, useState } from 'react'
 
 const btns = [
   {
@@ -49,91 +60,142 @@ const btns = [
 
 const Home = () => {
   const { total, setRating } = useRating()
+  const [lastClicked, setLastClicked] = useState<any>()
+
+  const handleClick = (value: number) => {
+    setRating(value)
+    setLastClicked(btns[value - 1])
+
+    setTimeout(() => {
+      setLastClicked(undefined)
+    }, 2000)
+  }
 
   return (
-    <div className='flex flex-col h-screen'>
-      <header className='z-10 flex items-center py-3 px-8 shadow-lg'>
-        <div className='w-20 h-20 overflow-hidden'>
-          <img
-            src={TuTgiLogo}
-            alt='Tu Tgi'
-            className='w-full h-full object-cover'
-          />
-        </div>
-        <div className='flex-1 flex justify-center'>
-          <h4 className='font-bold text-xl'>
-            ၂၀၂၅ ခုနှစ်၊ (၇၈)နှစ်မြောက်ရှမ်းပြည်နယ်နေ့အခမ်းအနား နှင့် Project
-            ပြပွဲ
-          </h4>
-        </div>
-        <div className='w-20 h-20 overflow-hidden'>
-          <img
-            src={ItLogo}
-            alt='Information Technology'
-            className='w-full h-full object-cover'
-          />
-        </div>
-      </header>
-      <main className='flex-1 gap-2 grid grid-cols-1 lg:grid-cols-3'>
-        <section className='flex items-center justify-center'>
-          <Card className='flex flex-col w-full max-w-[400px]'>
-            <CardHeader className='items-center'>
-              <CardTitle>Result</CardTitle>
-              <CardDescription>2024-2025</CardDescription>
-            </CardHeader>
-            <CardContent className='flex-1 pb-0'>
-              <Chart />
-            </CardContent>
-            <CardFooter className='flex-col items-start gap-2 text-sm'>
-              <div className='flex gap-2 font-medium leading-none'>
-                စုစုပေါင်း {total} ယောက်က မဲပေးခဲ့ပါသည်။
-              </div>
-            </CardFooter>
-          </Card>
-        </section>
+    <>
+      {/* <Dialog
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Candidate</DialogTitle>
+          </DialogHeader>
+          <DialogFooter>
+            <Button
+              variant='secondary'
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog> */}
 
-        <section className='flex items-center justify-center'>
-          <div>
-            <h1 className='text-center font-bold text-3xl mb-8 text-primary'>
-              ယနေ့ပွဲအပေါ်သင်၏သတ်မှတ်ချက်
-            </h1>
-            <div className='flex items-center gap-2'>
-              {btns.map(btn => (
-                <div
-                  key={btn.value}
-                  className='flex flex-col items-center'
-                >
-                  <button
-                    // onClick={() => {
-                    //   setRating(btn.value)
-                    // }}
-                    onPointerDown={() => {
-                      setRating(btn.value)
-                    }}
-                    className='hover:scale-125 duration-500'
-                  >
-                    <img
-                      src={btn.img}
-                      alt={btn.title}
-                      className='w-20 h-20 object-cover'
-                    />
-                  </button>
-                  <h5 className='text-sm font-bold'>{btn.title}</h5>
-                </div>
-              ))}
+      {lastClicked && (
+        <div className='fixed inset-0 z-50'>
+          <div className='absolute inset-0 bg-black/60'></div>
+          <div className='absolute inset-0 flex items-center justify-center'>
+            <div className='relative text-primary bg-primary-foreground inline-block w-[200px] p-4 rounded-lg'>
+              <h3 className='font-bold text-2xl text-center mb-4'>
+                {lastClicked.title}
+              </h3>
+              <div className='flex justify-center'>
+                <img
+                  src={lastClicked.img}
+                  alt={lastClicked.title}
+                  className='w-30 h-30 object-cover'
+                />
+              </div>
             </div>
           </div>
-        </section>
+        </div>
+      )}
 
-        <section className='w-full h-full p-4'>
-          <img
-            src={TuTgi}
-            alt='TU Taunggyi'
-            className='w-full h-full object-cover'
-          />
-        </section>
-      </main>
-    </div>
+      <div className='flex flex-col h-screen'>
+        <header className='z-10 flex items-center py-3 px-8 shadow-lg'>
+          <div className='w-20 h-20 overflow-hidden'>
+            <img
+              src={TuTgiLogo}
+              alt='Tu Tgi'
+              className='w-full h-full object-cover'
+            />
+          </div>
+          <div className='flex-1 flex justify-center'>
+            <h4 className='font-bold text-xl'>
+              ၂၀၂၅ ခုနှစ်၊ (၇၈)နှစ်မြောက်ရှမ်းပြည်နယ်နေ့အခမ်းအနား နှင့် Project
+              ပြပွဲ
+            </h4>
+          </div>
+          <div className='w-20 h-20 overflow-hidden'>
+            <img
+              src={ItLogo}
+              alt='Information Technology'
+              className='w-full h-full object-cover'
+            />
+          </div>
+        </header>
+        <main className='flex-1 gap-2 grid grid-cols-1 lg:grid-cols-3'>
+          <section className='flex items-center justify-center'>
+            <Card className='flex flex-col w-full max-w-[400px]'>
+              <CardHeader className='items-center'>
+                <CardTitle>Result</CardTitle>
+                <CardDescription>2024-2025</CardDescription>
+              </CardHeader>
+              <CardContent className='flex-1 pb-0'>
+                <Chart />
+              </CardContent>
+              <CardFooter className='flex-col items-start gap-2 text-sm'>
+                <div className='flex gap-2 font-medium leading-none'>
+                  စုစုပေါင်း {total} ယောက်က မဲပေးခဲ့ပါသည်။
+                </div>
+              </CardFooter>
+            </Card>
+          </section>
+
+          <section className='flex items-center justify-center'>
+            <div>
+              <h1 className='text-center font-bold text-3xl mb-8 text-primary'>
+                ယနေ့ပွဲအပေါ်သင်၏သတ်မှတ်ချက်
+              </h1>
+              <div className='flex items-center gap-2'>
+                {btns.map(btn => (
+                  <div
+                    key={btn.value}
+                    className='flex flex-col items-center'
+                  >
+                    <button
+                      // onClick={() => {
+                      //   setRating(btn.value)
+                      // }}
+                      onPointerDown={() => {
+                        handleClick(btn.value)
+                      }}
+                      className='hover:scale-125 duration-500'
+                    >
+                      <img
+                        src={btn.img}
+                        alt={btn.title}
+                        className='w-20 h-20 object-cover'
+                      />
+                    </button>
+                    <h5 className='text-sm font-bold'>{btn.title}</h5>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+
+          <section className='w-full h-full p-4'>
+            <img
+              src={TuTgi}
+              alt='TU Taunggyi'
+              className='w-full h-full object-cover'
+            />
+          </section>
+        </main>
+      </div>
+    </>
   )
 }
 
